417aa759f9ae7034b0c2102ddcb6e855bb04ba00
Jackson Mangallay <162795271+jacksondevgit@users.noreply.github.com>
2024-08-09 20:00:54 +0800

    refactor: updated eslint code and commands (#5)
    Co-authored-by: jackson <jackson@asus-rog-strix>

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  {
+    rules: {
+      'prefer-promise-reject-errors': 'off',
+      quotes: ['warn', 'single', { avoidEscape: true }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'vue/multi-word-component-names': ['warn'],
+    },
+  },
+]
