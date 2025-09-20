983d6e7fa372fc730dba38a547b3a2146cba20ac
Jackson Mangallay <jacksonmdev@outlook.com>
2025-09-20 19:08:23 +0800

    chore: updated boot loader (#14)

--- src/boot/global-components.ts
+++ src/boot/global-components.ts
@@ -2,7 +2,7 @@ import type { App } from 'vue'
 import * as CoreUiComponents from 'modules/base/ui'
 
 export default (app: App) => {
-    Object.entries(CoreUiComponents).forEach(([name, component]) => {
-      app.component(name, component)
-    })
+  Object.entries(CoreUiComponents).forEach(([name, component]) => {
+    app.component(name, component)
+  })
 }
--- src/boot/index.ts
+++ src/boot/index.ts
@@ -3,14 +3,17 @@ import type { App } from 'vue'
 type BootFn = (app: App) => Promise<void> | void
 
 export const loadBootFiles = async (app: App) => {
-  const bootFiles = import.meta.glob('./*.ts', { eager: true })
+  const bootFiles = import.meta.glob('./*.ts', { eager: false })
 
   for (const path in bootFiles) {
-    const bootModule = bootFiles[path]
-    const bootFn = (bootModule as any).default as BootFn
+    const bootFile = bootFiles[path]
+    if (typeof bootFile === 'function') {
+      const bootModule = await bootFile()
+      const bootFn = (bootModule as any).default as BootFn
 
-    if (typeof bootFn === 'function') {
-      await bootFn(app)
+      if (typeof bootFn === 'function') {
+        await bootFn(app)
+      }
     }
   }
 }
